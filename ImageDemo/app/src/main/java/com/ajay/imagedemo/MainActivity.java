21740d26d89de1af609884505d5eb4e74d59406a
Ajay Kapila <ajaykaps.18@gmail.com>
2018-10-01 18:52:19 -0400

    Completed ImageDemo

--- ImageDemo/app/src/main/java/com/ajay/imagedemo/MainActivity.java
+++ ImageDemo/app/src/main/java/com/ajay/imagedemo/MainActivity.java
@@ -2,8 +2,19 @@ package com.ajay.imagedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void newPic (View view){
+
+        Log.i("Info", "button Clicked");
+        ImageView image = (ImageView) findViewById(R.id.image1);
+        image.setImageResource(R.drawable.pexels);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
